1e3e5e0a0d767e76f99d4a7b42c874b5dffe926c
Blessmi K <kblessmi2001@gmail.com>
2023-03-30 22:23:45 +0530

    day3-billing page is done

--- script/billingpage.js
+++ script/billingpage.js
@@ -1,25 +1,12 @@
 let formData = document.getElementById("form")
-// formData.addEventListener("submit",userData)
-let lsData = JSON.parse(localStorage.getItem("checkout")) || []
-
-// function userData(ele)
-// { 
-//     ele.preventDefault()
-//     let fname = document.getElementById("f_name").value
-//     let lname = document.getElementById("l_name").value
-//     let address = document.getElementById("address").value
-//     let states = document.getElementById("states").value
-//     let zip = document.getElementById("zip").value
-//     let city = document.getElementById("city").value
-//     let assign_items = {fname,lname,address,states,zip,city}
-//     lsData.push(assign_items)
-   
-// }
+let lsData = JSON.parse(localStorage.getItem("checkout"))
+let form_div = document.getElementById("form_div")
+let unorder = document.getElementById("unorder")
 
 let division = document.getElementById("division")
 let ul = document.getElementById("unorder_list")
-for(let i=0;i<lsData.length;i++)
-{
+// for(let i=0;i<lsData.length;i++)
+// {
   let li1=document.createElement("li")
   let li2=document.createElement("li")
   let li3=document.createElement("li")
@@ -28,17 +15,90 @@ for(let i=0;i<lsData.length;i++)
   let li6=document.createElement("li")
   let li7=document.createElement("li")
 
-  li1.innerText= lsData[i].fname
-  li2.innerText= lsData[i].lname
-  li3.innerText= lsData[i].address
-  li4.innerText= lsData[i].city
-  li5.innerText= lsData[i].select
-  li6.innerText= lsData[i].zip
-  li7.innerText= lsData[i].states
+  li1.innerText= lsData.fname
+  li2.innerText= lsData.lname
+  li3.innerText= lsData.address
+  li4.innerText= lsData.city
+  li5.innerText= lsData.select
+  li6.innerText= lsData.zip
+  li7.innerText= lsData.states
   
-  ul.append(li1,li2,li3,li4,li5,li6,li7)
-  division.append(ul)
+  let fullName = lsData.fname + " " +lsData.lname
+  let userAddress = lsData.city +","+lsData.select+","+lsData.zip
+  ul.append(fullName,li3,userAddress,li7)
+  
+// }
+//   for(let j=0;j<lsData.length;j++)
+// {
+  let line1=document.createElement("li")
+  let line2=document.createElement("li")
+  let line3=document.createElement("li")
+  let line4=document.createElement("li")
+  let line5=document.createElement("li")
+  let line6=document.createElement("li")
+  let line7=document.createElement("li")
 
+  line1.innerText= lsData.fname
+  line2.innerText= lsData.lname
+  line3.innerText= lsData.address
+  line4.innerText= lsData.city
+  line5.innerText= lsData.select
+  line6.innerText= lsData.zip
+  line7.innerText= lsData.states
+  
+  let fullNam = lsData.fname + " " +lsData.lname
+  let userAddres = lsData.city +","+lsData.select+","+lsData.zip
+  unorder.append(fullNam,line3,userAddres,line7)
+  // console.log(fullName,line3,userAddress,line7)
+// }
+
+localStorage.setItem("checkout",JSON.stringify(lsData))
+
+
+
+
+function showDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  var otherDropdown = document.getElementById("dropdown1");
+  dropdown.style.display = "block";
+  otherDropdown.style.display = "none";
+  otherDropdown.style.color = red;
 
 }
-localStorage.setItem("checkout",JSON.stringify(lsData))
+
+function showDropdown1() {
+  var dropdown = document.getElementById("dropdown1");
+  var otherDropdown = document.getElementById("dropdown");
+  dropdown.style.display = "block";
+  otherDropdown.style.display = "none";
+}
+
+function showDropdown2() {
+  var dropdown = document.getElementById("dropdown2");
+  var otherDropdown = document.getElementById("dropdown1");
+  dropdown.style.display = "block";
+  otherDropdown.style.display = "none";
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const selectedShippingMethod = urlParams.get('shipping_method');
+
+// Update the selected shipping method in the billing page
+const selectedShippingMethodEl = document.getElementById('selected_shipping_method');
+selectedShippingMethodEl.textContent = selectedShippingMethod;
+
+let economyGround = document.getElementById("economy")
+let payment_id = document.getElementById("payment_id")
+economyGround.addEventListener("click",economy)
+function economy()
+{
+  let li1 = document.createElement("li")
+  let li2 = document.createElement("li")
+  let li3 = document.createElement("li")
+
+  li1.innerText = "Economy Ground"
+  li2.innerText = "Delivered In 3 - 7 Business Days."
+  li3.innerText = "$5.00"
+
+  payment_id.append(li1,li2,li3)
+}
--- script/checkoutpage.js
+++ script/checkoutpage.js
@@ -1,6 +1,6 @@
 let formData = document.getElementById("form")
 formData.addEventListener("submit",userData)
-let lsData = JSON.parse(localStorage.getItem("checkout")) || []
+let lsData = JSON.parse(localStorage.getItem("checkout"))
 
 function userData(ele)
 { 
@@ -9,10 +9,45 @@ function userData(ele)
     let lname = document.getElementById("l_name").value
     let address = document.getElementById("address").value
     let states = document.getElementById("states").value
+    let select = document.getElementById("select").value
     let zip = document.getElementById("zip").value
     let city = document.getElementById("city").value
-    let assign_items = {fname,lname,address,states,zip,city}
-    lsData.push(assign_items)
-    localStorage.setItem("checkout",JSON.stringify(lsData))
+    let assign_items = {fname,lname,address,states,select,zip,city}
+    // lsData.push(assign_items)
+    localStorage.setItem("checkout",JSON.stringify(assign_items))
     location.href="./billingpage.html"
+
 }
+
+
+
+const economyEl = document.getElementById('economy');
+const standardEl = document.getElementById('standard');
+const businessEl = document.getElementById('business');
+const overnightEl = document.getElementById('overnight');
+
+const handleShippingMethod = () => {
+  if (economyEl.checked) {
+    localStorage.setItem('shippingMethod', 'Economy Ground: $5.00');
+  } else if (standardEl.checked) {
+    localStorage.setItem('shippingMethod', 'Standard Ground: $8.00');
+  } else if (businessEl.checked) {
+    localStorage.setItem('shippingMethod', '2 Business Days: $13.00');
+  } else if (overnightEl.checked) {
+    localStorage.setItem('shippingMethod', 'Overnight: $25.00');
+  }
+};
+
+economyEl.addEventListener('click', handleShippingMethod);
+standardEl.addEventListener('click', handleShippingMethod);
+businessEl.addEventListener('click', handleShippingMethod);
+overnightEl.addEventListener('click', handleShippingMethod);
+
+
+
+// continue to billing js here 
+let contBill = document.getElementById(("btn"))
+contBill.addEventListener("click",()=>{
+
+    
+})
